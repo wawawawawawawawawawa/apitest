42b41f4bb475fb0e340941fb406d6e06d637f3a7
wawawawawawawawawawa <1746409650@qq.com>
2019-12-18 17:36:45 +0800

    接口框架

--- src/test/Groovy/com.winicssec/fourCourse/db/Case.groovy
+++ src/test/Groovy/com.winicssec/fourCourse/db/Case.groovy
@@ -1,4 +1,45 @@
 package com.winicssec.fourCourse.db
 
+import org.junit.Assert
+import spock.lang.Specification
 class Case extends Specification{
+    DataRepository dataRepository
+
+    void setup() {
+        dataRepository = new DataRepository()
+    }
+
+    def "should get user info successfully"(){
+        given: "no given"
+        when : "query user table to get info"
+        def userinfo = dataRepository.getUserInfo()
+        then:"should get user info"
+        userinfo.each {it -> println it.username + ":" + it.age + ":" + it.create_date }
+
+    }
+    def "should get user address successfully"(){
+        given:"no given"
+        when:"query user and address table"
+        def adderssInfo = dataRepository.getAddressByUserName(userName)
+        then:"should get correct user address info"
+        Assert.assertEquals(adderssInfo.address, address)
+        where:
+        userName | address
+        "TOM"    | "chengdu"
+        "DONE"    | "beijing"
+        "ECHO"    | "shanghai"
+        "MARY"    | "hangzhou"
+    }
+    def "should add user successfully"(){
+        given: "no given"
+        when: "add user"
+        dataRepository.addUser(userName, age)
+        then: "should get added user successfully"
+        Assert.assertEquals(dataRepository.getUser(userName).username, userName)
+        where:
+        userName | age
+        "MARY"   | 55
+    }
+
+
 }
